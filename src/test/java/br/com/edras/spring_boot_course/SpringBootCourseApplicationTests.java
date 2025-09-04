9eb2f57cf9afecb45dc9fcaea3c14c9cdfa9592d
edrassimoes <edrassimoes@gmail.com>
2025-09-04 14:21:30 -0300

    Project created

---
+++ src/test/java/br/com/edras/spring_boot_course/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edras.spring_boot_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
